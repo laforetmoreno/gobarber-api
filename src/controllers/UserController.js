936e69b365221c2e9c4b3920922c422d4479c3c5
moreno <laforetmoreno@gmail.com>
2020-03-25 18:35:16 -0300

    Fix update user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -42,48 +42,50 @@ class UserController {
   }
 
   async update(req, res) {
-    try {
-      const { email, oldPassword } = req.body;
-
-      const schema = Yup.object().shape({
-        name: Yup.string(),
-        email: Yup.string().email(),
-        oldPassword: Yup.string().min(6),
-        password: Yup.string()
-          .min(6)
-          .when('oldPassword', (oldPassword, field) =>
-            oldPassword ? field.required() : field
-          ),
-        confirmPassword: Yup.string().when('password', (password, field) =>
-          password ? field.required().findOne([Yup.ref('password')]) : field
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
         ),
-      });
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
 
-      if (!(await schema.isValid(req.body))) {
-        return res.status(401).json({ message: 'Validation fails' });
-      }
-
-      const user = await User.findByPk(req.userId);
-
-      if (email && email !== user.email) {
-        const userExists = await User.findOne({ where: { email } });
-
-        if (userExists) {
-          return res.status(400).json({ message: 'User already exists' });
-        }
-      }
-
-      if (oldPassword && !(await user.checkPassword(oldPassword))) {
-        return res.status(401).json({ message: 'Password does not match' });
-      }
-      // Error
-      const { id, name, provider } = await User.update(req.body);
-
-      return res.status(200).json({ id, name, provider });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({ message: 'Ops', error });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
     }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId);
+
+    return res.json({
+      id,
+      name,
+      email,
+      avatar,
+    });
   }
 }
 
